48d6eae9866f808a35f9cc4c194c1bdca07c9c00
Mark Veltzer <mark.veltzer@gmail.com>
2025-04-20 07:58:01 +0300

    pass cicd

---
+++ config/python.py
@@ -0,0 +1,21 @@
+console_scripts = [
+]
+dev_requires = [
+    "pypitools",
+]
+config_requires = [
+    "pyclassifiers",
+]
+install_requires = [
+]
+build_requires = [
+    "pydmt",
+    "pymakehelper",
+]
+test_requires = [
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "flake8",
+    "mypy",
+]
