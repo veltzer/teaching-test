64a7c7323ba97c4f0ca6b1601c40fc5eebef70f6
Mark Veltzer <mark.veltzer@gmail.com>
2025-05-13 18:34:59 +0300

    added more tests

--- tests/test_add.py
+++ tests/test_add.py
@@ -2,3 +2,6 @@ from add import add
 
 def test_add():
     assert add(2,2)==4
+
+def test_add_another():
+    assert add(3,3)==6
---
+++ tests/test_more.py
@@ -0,0 +1,4 @@
+from add import add
+
+def test_more():
+    assert add(4,5)==9
